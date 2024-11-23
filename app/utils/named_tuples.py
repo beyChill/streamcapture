f3c49cc3a95156ec0a3c7fde69913bd5e53c12fe
beyChill <74477178+beyChill@users.noreply.github.com>
2024-11-23 01:14:59 -0500

    improved compatibility

--- app/utils/named_tuples.py
+++ app/utils/named_tuples.py
@@ -1,13 +1,39 @@
-from typing import NamedTuple, Optional
+from pathlib import Path
+from typing import Any, NamedTuple
 
-class GetStreamerUrl(NamedTuple):
+
+class DbFollowBlock(NamedTuple):
+    write: bool
+    query: tuple[Any]
+
+
+class HlsQueryResults(NamedTuple):
     name_: str
     success: bool = False
-    url: str = ""
+    url: str | None = None
+    domain: str | None = None
     room_status: str = "offline"
-    status_code: Optional[int] = 200
+    status_code: int = 200
+
 
 class Streamer(NamedTuple):
     name_: str
     site_slug: str
     site: str
+
+
+class StreamerData(NamedTuple):
+    name_: str
+    site: str
+    url: str | None
+    domain: str | None
+    path_: Path
+    file: str
+    metadata: list
+    success: bool
+
+
+class StreamerWithPid(NamedTuple):
+    pid: int
+    streamer_name: str
+    site_name: str
