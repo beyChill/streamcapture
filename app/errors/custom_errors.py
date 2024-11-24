cb93dca8cc7f7894462d47ed3d49d8592cf826d5
beyChill <74477178+beyChill@users.noreply.github.com>
2024-11-23 22:37:25 -0500

    removed duplicated code

--- app/errors/custom_errors.py
+++ app/errors/custom_errors.py
@@ -11,18 +11,18 @@ class GetDataError(Exception):
     code: int
     loader: str
     pre_text: str = field(init=False)
-    ERROR_RESPONSE: Dict[str, str] = field(init=False)
+    error_dict: Dict[str, str] = field(init=False)
 
     def __post_init__(self):
         self.pre_text = f"{colored("[FAIL]",'red')} {strftime("%H:%M:%S")}: (http code: {self.code}) module: {self.loader} \n\t"
-        self.ERROR_RESPONSE = {
+        self.error_dict = {
             "429": f"Chaturbate {colored('API overan','red')} with too many request",
             "not200": f"Unable to retrieve stream data for {colored(self.name_,'red')}",
             "notfound": f"{colored(self.name_,'red')} is not a Chaturbate Streamer",
         }
 
     def __str__(self):
-        error:str = self.ERROR_RESPONSE.get(
+        error: str = self.error_dict.get(
             self.key_, colored("An unexpected error has occured", "red")
         )
         return self.pre_text + error
@@ -30,27 +30,27 @@ class GetDataError(Exception):
 
 @dataclass(slots=True)
 class CliErrors(Exception):
-    name_: str
+    name_: str | None
     key_: str
     hint: str
     pre_text: str = field(init=False)
-    ERROR_RESPONSE: Dict = field(init=False)
+    error_dict: Dict = field(init=False)
 
     def __post_init__(self):
         self.pre_text = (
-            f"{colored("[FAIL]") ,'red'}{strftime("%H:%M:%S")}: {self.hint} \n\t"
+            f"{colored("[FAIL]" ,'red')} {strftime("%H:%M:%S")}: {self.hint} \n\t"
         )
-        self.ERROR_RESPONSE = {
+        self.error_dict = {
             "input": f"Command missing {colored('model name','red')} and {colored('site abbreviation','red')}",
             "valid_chars": f"Model's name, {colored(self.name_, 'red')} contains invalid characters",
             "site_prompt": f"Unknown streaming site: {colored(self.name_,'red')}",
             "no_site": f"add {colored('streamer site', 'red')} to command",
             "chars_site": f"Only use alpha characters for site,{colored({self.name_},'red')}",
-            "input_table": f"include an option with command: <name date num>",
+            "input_table": "The sort option is flawed.",
         }
 
     def __str__(self):
-        error: str = self.ERROR_RESPONSE.get(
+        error: str = self.error_dict.get(
             self.key_, colored("An unexpected error has occured", "red")
         )
         return self.pre_text + error
