10ddd2a6f5d8a9fdf31a7afba6dcea0b96d2b4d6
beyChill <74477178+beyChill@users.noreply.github.com>
2024-11-23 22:46:52 -0500

    threading added

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+from threading import Thread
+from app.database.create_db import db_init
 from app.log.logger import init_logging
-from ui.commandline import Cli
+from app.online_status import run_online_status
+from app.ui.commandline import Cli
 
 
-if __name__ =="__main__":
+if __name__ == "__main__":
     init_logging()
-
+    db_init()
+    thread =Thread(target=run_online_status, daemon=True)
+    thread.start()
     Cli().cmdloop()
