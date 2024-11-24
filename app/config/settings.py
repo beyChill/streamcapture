118dc79ceee08118897f1a31cea0c8c6bdc564a4
beyChill <74477178+beyChill@users.noreply.github.com>
2024-11-23 22:32:44 -0500

    new settings

--- app/config/settings.py
+++ app/config/settings.py
@@ -1,9 +1,30 @@
+from datetime import datetime
 from functools import lru_cache
+from logging import INFO
+from pathlib import Path
+from typing import Any, ClassVar, Literal
+
 from pydantic_settings import BaseSettings
 
+
 class Settings(BaseSettings):
+    db_name: str = "stream_capture.sqlite3"
+    db_config: str = "createtables.sql"
+    db_folder: str = "database/db"
+    i_download_folder: str = "downloads/images"
+    v_download_folder: str = "downloads/videos"
+    parent_dir: Path = Path(__file__).parents[1]
+    DB_PATH: Path = Path(parent_dir, db_folder, db_name)
+    DB_TABLES: Path = Path(parent_dir, Path(db_folder).parent, db_config)
+    IMAGE_DIR: Path = Path(parent_dir, i_download_folder)
+    VIDEO_DIR: Path = Path(parent_dir, v_download_folder)
+    video_length_seconds: int = 1800
+    CAPTURE_LENGTH: str = f"{video_length_seconds}"
     default_cli_prompt: str = "$"
+    log_level: Literal[20] = INFO
+    datetime: ClassVar = datetime.now().replace(microsecond=0)
+
 
 @lru_cache()
-def get_settings(**kwargs: dict) -> Settings:
+def get_settings(**kwargs) -> Settings:
     return Settings(**kwargs)
